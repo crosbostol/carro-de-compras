972e86febdeb5a62a0ee750240d2e0e4482bf4a6
Crostobol <cristobal.oyarzun@cloud.uautonoma.cl>
2022-11-09 16:42:23 -0300

    Carrito de compra

---
+++ src/components/Title.js
@@ -0,0 +1,30 @@
+import {Component } from 'react';
+
+const styles ={
+    titulo:{
+        marginBottom: '30px',
+
+
+    },
+
+
+}
+
+
+class Title extends Component{
+    render(){
+        return(
+            <h1  style={styles.titulo}>Tienda Mágica</h1>
+
+        )
+    }
+
+
+    
+
+
+
+
+}
+
+export default Title;
